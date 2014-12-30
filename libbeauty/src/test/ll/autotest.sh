e73da1a9503353db54d6c983e0183f78e2bc8c7d
James Courtier-Dutton <James@superbug.co.uk>
2014-12-30 14:57:14 +0000

    Added missing tests to autotest.sh

--- libbeauty/src/test/ll/autotest.sh
+++ libbeauty/src/test/ll/autotest.sh
@@ -6,6 +6,7 @@ export ENABLE_DEBUG_ANALYSE=0
 export ENABLE_DEBUG_ANALYSE_PATHS=0
 export ENABLE_DEBUG_ANALYSE_PHI=0
 export ENABLE_DEBUG_OUTPUT=0
+export ENABLE_DEBUG_OUTPUT_LLVM=0
 
 echo -n test10_ll:
 if ../../../test/dis64 test10_ll.o 2>&1 | grep -q "END - FINISHED PROCESSING"
@@ -42,3 +43,25 @@ then
 else
 	echo "FAILED"
 fi
+echo -n test15_ll:
+if ../../../test/dis64 test15_ll.o 2>&1 | grep -q "END - FINISHED PROCESSING"
+then
+	echo "PASSED"
+else
+	echo "FAILED"
+fi
+echo -n test16_ll:
+if ../../../test/dis64 test16_ll.o 2>&1 | grep -q "END - FINISHED PROCESSING"
+then
+	echo "PASSED"
+else
+	echo "FAILED"
+fi
+echo -n test17_ll:
+if ../../../test/dis64 test17_ll.o 2>&1 | grep -q "END - FINISHED PROCESSING"
+then
+	echo "PASSED"
+else
+	echo "FAILED"
+fi
+
